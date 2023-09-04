8aa0edf5b98fdaca2173cbeafcdfd67db1067e09
Sandor Madacsi <smadacsi@protonmail.com>
2023-09-04 14:05:39 +0100

    Implement calculation on operation button callback
    The calculation allgorithm was initially implemented in the equal button
    callback. Now the calucation is also called when the user selects second
    operator insetad of using the equal. This way the operations are chained.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
-let firstNum = null;
-let secNum = null;
+let firstNum = 0;
+let secNum = 0;
 let operatorSign = '';
 
 let placeHolder = document.querySelector('.place-holder');
@@ -9,7 +9,7 @@ let input = document.querySelector('.input');
 
 const nums = document.querySelectorAll('.num');
 nums.forEach( num => num.addEventListener('click', function(){
-    if(placeHolder.innerText === '0')
+    if(placeHolder.innerText === '0' || placeHolder.innerText === 'ERROR')
     {
         placeHolder.innerText = this.innerText;
     }
@@ -21,26 +21,55 @@ nums.forEach( num => num.addEventListener('click', function(){
 
 ));
 
+
 const operators = document.querySelectorAll('.op');
 operators.forEach(operator => operator.addEventListener('click', function(){
-    firstNum = Number(placeHolder.innerText);
-    operatorSign = this.innerText;
-    input.innerText = `${firstNum}  ${operatorSign}`;
-    placeHolder.innerText = '';
+    if(firstNum !== 0)
+    {
+        secNum = Number(placeHolder.innerText);
+        input.innerText = `${firstNum}  ${operatorSign} ${secNum}`;
+        operate(firstNum, secNum, operatorSign);
+        placeHolder.innerText = firstNum;
+        operatorSign = this.innerText;
+        input.innerText = `${firstNum}  ${operatorSign}`;
+        placeHolder.innerText = '';
+    }
+    else{
+        firstNum = Number(placeHolder.innerText);
+        operatorSign = this.innerText;
+        input.innerText = `${firstNum}  ${operatorSign}`;
+        placeHolder.innerText = '';
+    }
+    
+
 }));
 
 let equal = document.querySelector('.equal');
 equal.addEventListener('click', function(){
-    secNum = Number(placeHolder.innerText);
-    input.innerText = `${firstNum}  ${operatorSign} ${secNum}`;
-    operate(firstNum, secNum, operatorSign);
-    placeHolder.innerText = firstNum;
+    if(firstNum === 0 && secNum === 0)
+    {
+        console.log("Hello");
+        placeHolder.innerText = "ERROR";
+    }
+    else{
+        secNum = Number(placeHolder.innerText);
+        input.innerText = `${firstNum}  ${operatorSign} ${secNum}`;
+        operate(firstNum, secNum, operatorSign);
+        placeHolder.innerText = firstNum;
+    }
+
+
 
 });
 
 let clear = document.querySelector('.clear');
 clear.addEventListener('click', function(){
-    placeHolder.innerText = '';
+    placeHolder.innerText = '0';
+    input.innerText = '0';
+    operatorSign = '';
+    firstNum = 0;
+    secNum = 0;
+
 })
 
 
@@ -48,15 +77,23 @@ function operate(a,b,o){
     switch (o) {
         case '+':
            firstNum = add(a,b);
+           secNum = 0;
+           operatorSign = '';
             break;
         case '-':
            firstNum = subtract(a,b);
+           secNum = 0;
+           operatorSign = '';
             break;
         case 'x':
           firstNum =  multiply(a,b);
+          secNum = 0;
+          operatorSign = '';
             break;
         case '/':
-          firstNum =  divide(a,b);    
+          firstNum =  divide(a,b);
+          secNum = 0;
+          operatorSign = '';
             break;
         default:
             break;
@@ -76,5 +113,6 @@ function multiply(a,b){
 }
 
 function divide(a,b){
+    if(b === 0) return 'ERROR'
     return a / b;
 }
